8c3a926dc1624b2b0a13c9bf1475ab8ba76b9869
seyio91 <seyi_obaweya@yahoo.com>
2019-12-28 06:31:29 +0200

    Added Formatting

--- safedelete.sh
+++ safedelete.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
+blue="\e[96m"
+normal="\033[0m"
+greentext="\033[32m"
+warning="\e[93m"
+danger="\e[91m"
 SAFEDELETEDIR="$HOME/.Trash"
 RECURSIVE_DEL=
 DIRONLY_DEL=
 INTERACTIVE_DEL=
 FILEONLY_DEL=
 emptyargs=true
-#variable to store multiple files is many files are passed to command
 FILES=
 EXIT_CODE=0
 
+echo 
 usage(){
-  echo "usage: ./solution.sh [-i | -d | -r] file folder ....."
+  echo -e $warning"Usage: ./safedelete.sh [ -i | -d | -r | -f ] File ..."
   echo " -i : Interactive Prompt"
   echo " -d : Directory Delete Only"
-  echo " -r : File and Dir"
-  echo " -f : File Delete only"
+  echo " -r : File and Dir Delete"
+  echo -e " -f : File Delete only"$normal
+  echo
   exit 1
 }
 
@@ -37,13 +43,12 @@ shift $((OPTIND -1))
 if [[ $emptyargs = true ]]; then
     usage
 fi
-
-#only run when other checks pass
+#pass remaining arguments to files
 FILES=("$@")
 
 #check if Files are not empty after argument
 if [[ ! ${FILES[@]} ]]; then
-    echo "File Argument Missing in Command"
+    echo -e $warning"Error: No File Specified to be deleted!!!"$normal
     usage
 fi
 
@@ -51,7 +56,7 @@ fi
 delete(){
     local file=$1
     if [[ $INTERACTIVE_DEL = 1 ]]; then
-        echo "Are You Sure you want to delete $file"
+        echo -e $blue"Are You Sure you want to Delete \"$file\"?"$normal
         echo -n "(yes/no): "
 
         read reply
@@ -59,7 +64,7 @@ delete(){
         if [[ $reply = "yes" || $reply = "y" || ! -n $reply ]]; then
             trash $file
         else
-            echo -e "Cancelled\n"
+            echo -e $danger"Cancelled by User!!\n"$normal
         fi
     else
         trash $file
@@ -77,23 +82,22 @@ trash(){
 
 
     #check to see if previous versions already exist in trashdir
-    #find a better way to make unique
+    #find a better way to make unique from test.txt.23-12-2019-19:24:38
     if [[ -e $trashname ]]; then 
         echo "Found in trash, renaming as $trashname.$(date +%d-%m-%Y-%T)"
         trashname="$trashname.$(date +%d-%m-%Y-%T)"
-        #this returns test.txt.23-12-2019-19:24:38
     fi
 
-    #Moving to TrashDirectory
+    #Moving to TrashDirectory. Change to mv command
     cp -r "$fullpath" "$trashname"
-    echo -e "Deleting File: $file .....\n"
+    echo -e $greentext"Deleting \"$file\" .....\n"$normal
     return 0
 }
 
 
 #ensure SafeDelete Folder Exists
 if [[ ! -e $SAFEDELETEDIR ]]; then
-    echo "Safe Delete Recycle Bin does not Exist, Do you want to create it at \"$SAFEDELETEDIR\"? :"
+    echo -e $blue"Safe Delete Recycle Bin does not Exist, Do you want to create it at \"$SAFEDELETEDIR\"?: "$normal
     echo -n "(yes/no): "
 
     read reply
@@ -101,7 +105,7 @@ if [[ ! -e $SAFEDELETEDIR ]]; then
     if [[ $reply = "yes" || $reply = "y" || ! -n $reply ]]; then
         mkdir -p "$SAFEDELETEDIR"
     else
-        echo "Exiting SafeDelete. SafeDelete Recycle Bin does not Exist"
+        echo -e $danger"Exiting Script. SafeDelete Recycle Bin does not Exist"$normal
         exit 1
     fi
 fi
@@ -110,7 +114,7 @@ fi
 for file in "${FILES[@]}"; do
     #test if file Exist
     if [[ ! -e $file ]]; then
-        echo -e "File $file does not Exist\n"
+        echo -e $warning"\"$file\" does not Exist\n"$normal
         EXIT_CODE=1
         continue
     fi
@@ -118,41 +122,38 @@ for file in "${FILES[@]}"; do
 
     #check if file is not main directory
     if [[ $file = "/" ]]; then
-        echo "it is dangerous to operate recursively on /, exiting script"
+        echo -e $danger"it is dangerous to operate recursively on /, exiting script"$normal
         exit 1
     fi
 
     #note places i am setting exit_code to 1 without calling
     #i want for loop to finish and if that was the only file,
     #the exit is called at the end of the script. this allows
-    #you to skip invalid file and script work for the valids ones
+    #you to skip invalid file and the script work for the valids ones
+
     #check if File is . or ..
     if [[ $file = "." || $file = ".." ]]; then
-        echo "$COMMAND: \".\" and \"..\" may not be removed"
+        echo -e $warning"$COMMAND: \".\" and \"..\" may not be removed"$normal
         EXIT_CODE=1
         continue
     fi
 
     #the same check also apply on /. /.. if basename
     if [[ $(basename $file) = "." || $(basename $file) = ".." ]]; then
-        echo "$COMMAND: \".\" and \"..\" may not be removed"
+        echo -e $warning"$COMMAND: \".\" and \"..\" may not be removed"$normal
         echo
         EXIT_CODE=1
         continue
     fi
 
-    #add other conditions to Eliminate here
-    # if [[ $INTERACTIVE_DEL = 1 ]]; then
 
-    # fi
-
-    #if $RECURSIVE_DEL is set. Delete Dir or File without Caring If anything is inside
+    #if $RECURSIVE_DEL is set. Delete Dir or File irrespective its a file or directory
     if [[ -n $RECURSIVE_DEL ]]; then 
         delete "$file"
     else
         #check if file is Folder
         if [[ -d $file ]] && [[ ! -n $DIRONLY_DEL ]]; then
-            echo -e "Error Folder cannot be Deleted as recursive command is not passed and file is folder\n"
+            echo -e $warning"Error Folder cannot be Deleted as recursive command is not passed and file is folder\n"$normal
             EXIT_CODE=1
             continue
         fi
