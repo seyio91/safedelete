96ea92385bd8cf5cfa25292367bd2f6e980cf968
seyio91 <seyi_obaweya@yahoo.com>
2019-12-28 05:38:48 +0200

    Added Interactive Prompts

--- safedelete.sh
+++ safedelete.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 SAFEDELETEDIR="$HOME/.Trash"
 RECURSIVE_DEL=
-FILEONLY_DEL=
 DIRONLY_DEL=
+INTERACTIVE_DEL=
+FILEONLY_DEL=
 emptyargs=true
 #variable to store multiple files is many files are passed to command
 FILES=
 EXIT_CODE=0
 
 usage(){
-  echo "usage: ./solution.sh [-f | -d | -r] file folder ....."
-  echo " -f : File Only"
-  echo " -d : Directory Only Only"
-  echo " -r : File and Dir Only"
+  echo "usage: ./solution.sh [-i | -d | -r] file folder ....."
+  echo " -i : Interactive Prompt"
+  echo " -d : Directory Delete Only"
+  echo " -r : File and Dir"
+  echo " -f : File Delete only"
   exit 1
 }
 
 
-while getopts "fdr" option; do
+while getopts "idrf" option; do
     case "$option" in
 
-    f ) FILEONLY_DEL=1;;
+    i ) INTERACTIVE_DEL=1;;
     d ) DIRONLY_DEL=1;;
     r ) RECURSIVE_DEL=1;;
+    f ) FILEONLY_DEL=1;;
     \? ) usage
         ;;
     esac
@@ -39,10 +42,29 @@ fi
 FILES=("$@")
 
 #check if Files are not empty after argument
-if [ ! ${FILES[@]} ]; then
+if [[ ! ${FILES[@]} ]]; then
     echo "File Argument Missing in Command"
     usage
 fi
+
+#wrapper for prompts
+delete(){
+    local file=$1
+    if [[ $INTERACTIVE_DEL = 1 ]]; then
+        echo "Are You Sure you want to delete $file"
+        echo -n "(yes/no): "
+
+        read reply
+
+        if [[ $reply = "yes" || $reply = "y" || ! -n $reply ]]; then
+            trash $file
+        else
+            echo -e "Cancelled\n"
+        fi
+    else
+        trash $file
+    fi
+}
 
 trash(){
     local file=$1
@@ -51,6 +73,8 @@ trash(){
     local base=$(basename "$file")
     local fullpath=$(realpath "$file")
     trashname="$SAFEDELETEDIR/$base"
+
+
 
     #check to see if previous versions already exist in trashdir
     #find a better way to make unique
@@ -61,16 +85,32 @@ trash(){
     fi
 
     #Moving to TrashDirectory
-    cp "$fullpath" "$trashname"
+    cp -r "$fullpath" "$trashname"
+    echo -e "Deleting File: $file .....\n"
     return 0
 }
 
+
+#ensure SafeDelete Folder Exists
+if [[ ! -e $SAFEDELETEDIR ]]; then
+    echo "Safe Delete Recycle Bin does not Exist, Do you want to create it at \"$SAFEDELETEDIR\"? :"
+    echo -n "(yes/no): "
+
+    read reply
+
+    if [[ $reply = "yes" || $reply = "y" || ! -n $reply ]]; then
+        mkdir -p "$SAFEDELETEDIR"
+    else
+        echo "Exiting SafeDelete. SafeDelete Recycle Bin does not Exist"
+        exit 1
+    fi
+fi
 
 #FILE DELETE
 for file in "${FILES[@]}"; do
     #test if file Exist
     if [[ ! -e $file ]]; then
-        echo "File $file does not Exist"
+        echo -e "File $file does not Exist\n"
         EXIT_CODE=1
         continue
     fi
@@ -96,29 +136,29 @@ for file in "${FILES[@]}"; do
     #the same check also apply on /. /.. if basename
     if [[ $(basename $file) = "." || $(basename $file) = ".." ]]; then
         echo "$COMMAND: \".\" and \"..\" may not be removed"
+        echo
         EXIT_CODE=1
         continue
     fi
 
     #add other conditions to Eliminate here
+    # if [[ $INTERACTIVE_DEL = 1 ]]; then
 
+    # fi
 
     #if $RECURSIVE_DEL is set. Delete Dir or File without Caring If anything is inside
     if [[ -n $RECURSIVE_DEL ]]; then 
-        echo echo "Deleting File: $file ....."
-        trash "$file"
-        continue
+        delete "$file"
     else
         #check if file is Folder
         if [[ -d $file ]] && [[ ! -n $DIRONLY_DEL ]]; then
-            echo "Error Folder cannot be Deleted as recursive command is not passed and file is folder"
+            echo -e "Error Folder cannot be Deleted as recursive command is not passed and file is folder\n"
             EXIT_CODE=1
             continue
         fi
 
         #Function to Delete What is Left
-        echo "Deleting File: $file ....."
-        trash "$file"
+        delete "$file"
     fi
 
 done
